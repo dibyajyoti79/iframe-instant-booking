e481e657ba9b9a4428f30c9b89667570009fe481
dibyajyoti79 <dibyajyoti274@gmail.coom>
2023-10-17 14:39:38 +0530

    fix: application error

--- src/react-app-env.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "*.png";
-declare module "*.svg";
-declare module "*.jpeg";
-declare module "*.jpg";
-declare module 'html2pdf.js' {
-    const html2pdf: any;
-    export default html2pdf;
-}
